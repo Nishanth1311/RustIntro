4f27f234090a8570e3cd8207bb27b91f851bb2a7
NishanthSanjeevi <57482332+NishanthSanjeevi@users.noreply.github.com>
2023-09-21 04:22:43 +0530

    Add ownership and borrowing

---
+++ borrowing/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // You own a car
+    let mut your_car = String::from("Your Car");
+
+    // Your friend wants to borrow your car and modify it
+    let mut borrowed_car = &mut your_car;
+
+    // Your friend can modify it temporarily
+    borrowed_car.push_str("'s Color");
+
+    // You can't access your car while your friend has borrowed it
+    // Uncommenting the following line would result in a compilation error:
+    // your_car.push_str("'s Model"); // Error: Cannot borrow `your_car` as mutable because it is also borrowed as mutable
+    println!("Your friend is modifying: {}", borrowed_car);
+
+    // Once your friend is done, you can use it again
+    your_car.push_str("'s Model");
+    println!("You own: {}", your_car);
+}
---
+++ ownership/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    // person1 owns a string
+    let mut person1: String = String::from("I own Coca-Cola");
+
+    // person2 clones what person1 has and appends " new"
+    let mut person2: String = person1.clone() + " new";
+
+    // Now, person1 and person2 both have coca-cola
+    println!("Person1: {}", person1); // Output: Person1: I own Coca-Cola
+    println!("Person2: {}", person2); // Output: Person2: I own Coca-Cola new
+    
+    // person1 loses his business as everyone goes to Person2
+    person1.clear();
+
+    // Now, person1 and person2 both have coca-cola
+    println!("Person1: {}", person1); // Output: Person1: (owns nothing)
+    println!("Person2: {}", person2); // Output: Person2: I own Coca-Cola new
+    
+
+    // person1 is mad and takes ownership back
+    person1 = person2.clone();
+
+    // person2 clears their string
+    person2.clear();
+
+    // Now, person1 has the modified string, and person2's string is cleared
+    println!("Person1: {}", person1); // Output: Person1: I own Coca-Cola new
+    println!("Person2: {}", person2); // Output: Person2: (owns nothing)
+}
